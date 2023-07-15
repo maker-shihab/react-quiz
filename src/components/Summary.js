6937be3b7cd54022159e9bd735a3bc1763e5a55d
Shihab <73121069+maker-shihab@users.noreply.github.com>
2023-07-15 13:25:52 +0600

    lesson-5 Update Result page with summary and analysis components

---
+++ src/components/Summary.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import SuccessImage from '../assets/images/success.png';
+import classes from '../styles/Summary.module.css';
+
+const Summary = () => {
+  return (
+    <div className={classes.summary}>
+      <div className={classes.point}>
+        <p className={classes.score}>
+          Your score is <br />
+          5 out of 10
+        </p>
+      </div>
+
+      <div className={classes.badge}>
+        <img src={SuccessImage} alt="Success" />
+      </div>
+    </div>
+  );
+};
+
+export default Summary;
