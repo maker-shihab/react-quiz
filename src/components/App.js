d2349bd36f2caf9a1060efc7b4c92812e25d5e95
Shihab <73121069+maker-shihab@users.noreply.github.com>
2023-07-16 09:24:49 +0600

    firabase authentication create email and password

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
+import { AuthProfider } from '../contexts/AuthContext';
 import "../styles/App.css";
 import Layout from './Layout';
 import Home from './pages/Home';
@@ -8,19 +9,20 @@ import Quiz from './pages/Quiz';
 import Result from './pages/Result';
 import Signup from './pages/Signup';
 
-
 const App = () => {
   return (
     <Router>
-      <Layout>
-        <Switch>
-          <Route exact path='/' component={Home} />
-          <Route exact path='/signup' component={Signup} />
-          <Route exact path='/login' component={Login} />
-          <Route exact path='/quiz' component={Quiz} />
-          <Route exact path='/result' component={Result} />
-        </Switch>
-      </Layout>
+      <AuthProfider>
+        <Layout>
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/signup' component={Signup} />
+            <Route exact path='/login' component={Login} />
+            <Route exact path='/quiz' component={Quiz} />
+            <Route exact path='/result' component={Result} />
+          </Switch>
+        </Layout>
+      </AuthProfider>
     </Router>
   );
 };
